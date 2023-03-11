3fec9956c6543f7ae3f8cdfa0a3f4f461c31aa11
ragamadhuri07 <113750216+ragamadhuri07@users.noreply.github.com>
2023-03-11 15:20:43 +0530

    Delete 9-3-23 question.py

--- 9-3-23 question.py
+++
@@ -1,7 +0,0 @@
-#program:
-# create two stacks:
-# define size of both stacks as 5
-# if your input is even number it has to be pushed
-# if stack_1 and other inputs has to be pushed
-# in stack_2
-# options : push/pop/display_stack/quit
